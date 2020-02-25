89dc7952a09fd397dff693d25a2eb6adac233fb3
Kebikau Kiryl <kebikov1995@gmail.com>
2020-02-25 20:24:09 +0300

    feat: add functional in method createDreameTeam()

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+
+  if (!Array.isArray(array) || array.length === 0) {
+    return false;
+  }
+
+  let str = array.reduce( function(s, i) {
+
+    if(typeof i == "string") s = s + i.trim()[0];
+    return s;
+  }, "");
+  str = str.split('').sort().join('').toUpperCase();
+  return str.split('').sort().join('').toUpperCase();
 };
